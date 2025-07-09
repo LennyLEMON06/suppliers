c6356fd05b09bbdf737902e98685e47717ec46e4
LEMON <115541669+LennyLEMON06@users.noreply.github.com>
2025-07-09 12:45:35 +0300

    Update urls.py

--- procurement_service/procurement/urls.py
+++ procurement_service/procurement/urls.py
@@ -17,12 +17,19 @@ urlpatterns = [
     # Итоговые таблицы
     path('product_supplier_table/', views.product_supplier_table, name='product_supplier_table'),
     path('prices/', views.price_list, name='price_list'),
+
+    # Category
+    path('category_list/', views.category_list, name='category_list'),
+    path('add_category/', views.add_category, name='add_category'),
+    path('edit_category/<int:category_id>/', views.edit_category, name='edit_category'),
+    path('delete_category/<int:category_id>/', views.delete_category, name='delete_category'),
     
     # Supplier
     path('add_supplier/', views.add_supplier, name='add_supplier'),  
     path('edit_supplier/<int:supplier_id>/', views.edit_supplier, name='edit_supplier'),
     path('delete_supplier/<int:supplier_id>/', views.delete_supplier, name='delete_supplier'),
     path('supplier_list/', views.supplier_list, name='supplier_list'),
+    path('toggle_supplier_visibility/<int:supplier_id>/', views.toggle_supplier_visibility, name='toggle_supplier_visibility'),
     
     # Product
     path('add_product/', views.add_product, name='add_product'),
@@ -34,4 +41,10 @@ urlpatterns = [
     path('upload/', views.upload_excel, name='upload_excel'),
     path("export-prices/", views.export_prices_to_excel, name="export_prices"),
     path('export/', views.export_to_excel, name='export_to_excel'),
+
+    path('product-supplier-table/', views.product_supplier_table, name='product_supplier_table'),
+    
+    path('best-prices-by-category/', views.best_prices_by_category, name='best_prices_by_category'),
+    path('price-history/<int:product_id>/', views.price_history_view, name='price_history'),
+
 ]
